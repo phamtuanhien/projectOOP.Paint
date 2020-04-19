95259cebdeee6f0f35c670b078d92fac62d0757c
hoanganhlc14600 <anh.nh180010@sis.hust.edu.vn>
2020-04-19 18:20:22 +0700

    Create DrawMethod.java

---
+++ shape/DrawMethod.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shape;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author hoang
+ */
+public interface DrawMethod {
+    public void draw(Graphics2D g2d);
+}
