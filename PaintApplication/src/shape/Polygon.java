6e22c54a4ad59d5fabf42343f964357740b32fa2
dai02012000 <ledai02012000@gmail.com>
2020-04-21 18:25:12 +0700

    a

--- PaintApplication/src/shape/Polygon.java
+++ PaintApplication/src/shape/Polygon.java
@@ -8,29 +8,9 @@ package shape;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
-/**
-<<<<<<< HEAD
- *
- * @author Đại
- */
-public class Polygon extends Shape implements DrawType{
-    // tạo danh sách chứa đường thẳng
-    private ArrayList<Line> listLine = new ArrayList<Line>();
 
-    public ArrayList<Line> getListLine() {
-        return listLine;
-    }
-    
-    // Thêm 1 đường thẳng vào danh sách
-    public void addLine(Line line){
-        listLine.add(line);
-    }
-    
-    @Override
-    public void draw(Graphics2D g2d) {
-        for (int i = 0; i < listLine.size(); i++) {
-=======
- * hinh da giac
+ /**       
+ * hình đa giác
  * @author Phuc
  */
 public class Polygon extends Shape implements DrawType {
@@ -47,10 +27,9 @@ public class Polygon extends Shape implements DrawType {
 
     @Override
     public void draw(Graphics2D g2d) {
-        g2d.setColor(strokeColor);
+        
         for(int i=0;i<listLine.size();i++)
         {
->>>>>>> e971cd651f07976b5687e35634aa3892c8e7b208
             listLine.get(i).draw(g2d);
         }
     }
