3ca9100ca93cd06cf4175be8b7988936b42142b8
hung.nd <57054734+nguyen-dang-hung-20183547@users.noreply.github.com>
2020-04-23 14:42:56 +0700

    create bucket curve eraser

--- PaintApplication/src/shape/Bucket.java
+++ PaintApplication/src/shape/Bucket.java
@@ -13,20 +13,14 @@ import java.util.ArrayList;
  * @author Dang Hung
  */
 public class Bucket extends Shape implements DrawType{
-    private Point click;// diem click vao vung de to mau
     private Color color;// mau duoc chon de to 
     private int color_fill ;//mau duoc chon de to dinh dang int
     private int color_before;//mau truoc khi to dinh dang int
-    private ArrayList <Point> arrPoint = new ArrayList<Point>();
     //getter and setter
-    public Point getClick() {
-        return click;
+    public void setStart(Point start){
+        this.startPoint = start; 
     }
-
-    public void setClick(Point click) {
-        this.click = click;
-    }
-
+        
     public Color getColor() {
         return color;
     }
@@ -34,17 +28,9 @@ public class Bucket extends Shape implements DrawType{
     public void setColor(Color color) {
         this.color = color;
     }
-
-    public ArrayList<Point> getArrPoint() {
-        return arrPoint;
-    }
-
-    public void setArrPoint(Point point) {
-        this.arrPoint.add(point);
-    }
     //overloading draw
     public void draw(BufferedImage image){
-        color_before = image.getRGB(click.x, click.y);//tao mau ban dau cua diem anh
+        color_before = image.getRGB(startPoint.x, startPoint.y);//tao mau ban dau cua diem anh
         color_fill = this.color.getRGB();//tao mau muon to
         spreadFill(image);
         
@@ -55,8 +41,8 @@ public class Bucket extends Shape implements DrawType{
         if(color_before == color_fill)//check color tai diem xet neu da to dung mau thi dung
             return;
         else {
-            image.setRGB(click.x, click.y, color_fill);//to cho diem anh toa do click
-            listPoints.add(click);//them click vao dau cua hang doi de thuc hien loang tu click
+            image.setRGB(startPoint.x, startPoint.y, color_fill);//to cho diem anh toa do click
+            listPoints.add(startPoint);//them click vao dau cua hang doi de thuc hien loang tu click
             while(!listPoints.isEmpty()){
                 //lay ra phan tu dau danh sach de xet va xet toi khi hang doi rong
                 // thuat toan giong bfs tren do thi
@@ -67,7 +53,7 @@ public class Bucket extends Shape implements DrawType{
                 //(x-1,y)                   (x+1,y)
                 //               (x,y+1)     
                 //image.setRGB(Point) == color_before: check xem diem anh co phai canh da giac hay k
-                
+                //cac phep so sanh de tranh bug tran vien
                 if (pointEx.x - 1 >= 0 && image.getRGB(pointEx.x - 1, pointEx.y) == color_before) {
                     image.setRGB(pointEx.x - 1, pointEx.y, color_fill);
                     listPoints.add(new Point(pointEx.x - 1, pointEx.y));
@@ -76,11 +62,11 @@ public class Bucket extends Shape implements DrawType{
                     image.setRGB(pointEx.x, pointEx.y - 1, color_fill);
                     listPoints.add(new Point(pointEx.x, pointEx.y - 1));
                 }
-                if (image.getRGB(pointEx.x, pointEx.y + 1) == color_before) {
+                if (pointEx.y+1 <= image.getHeight()-1 && image.getRGB(pointEx.x, pointEx.y + 1) == color_before) {
                     image.setRGB(pointEx.x, pointEx.y + 1, color_fill);
                     listPoints.add(new Point(pointEx.x, pointEx.y + 1));
                 }
-                if (image.getRGB(pointEx.x+1, pointEx.y ) == color_before) {
+                if (pointEx.x+1 <= image.getWidth()-1 && image.getRGB(pointEx.x+1, pointEx.y ) == color_before) {
                     image.setRGB(pointEx.x+1, pointEx.y , color_fill);
                     listPoints.add(new Point(pointEx.x+1, pointEx.y ));
                 }
